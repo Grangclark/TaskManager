15da2e1371e9abeedc85fd0563faf3aa3e586b4a
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-01-27 23:51:50 +0900

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by 長橋和敏 on 2025/01/27.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
